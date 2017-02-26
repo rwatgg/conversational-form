471ab23f9c17188474e57b75880e8d2d2633773d
Felix Nielsen <felix@flexmotion.dk>
2017-02-26 15:27:39 +0000

    bookmark init

---
+++ src/scripts/cf/ConversationalBookmarklet.js
@@ -0,0 +1,7 @@
+javascript(function(){
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.crossorigin = 'crossorigin';
+	script.src = 'https://conversational-form-0iznjsw.stackpathdns.com/conversational-form.min.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
